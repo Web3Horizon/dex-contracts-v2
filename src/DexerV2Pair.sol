c1144e092e228b3d530d6df17b96d5f4a8ded80b
Matheus-OAMK <t2fema00@students.oamk.fi>
2024-11-29 20:09:50 +0200

    feat: implement LP burn functionality for DexerV2Pair
    - Added `burn` function to allow LP token holders to remove liquidity from the pool.
    - The function burns LP tokens and transfers the corresponding amounts of `token0` and `token1` to a specified address.

--- src/DexerV2Pair.sol
+++ src/DexerV2Pair.sol
@@ -14,12 +14,12 @@ contract DexerV2Pair is ERC20 {
     uint256 private reserve1;
 
     /* **** Events **** */
-
+    event Burn(address sender, uint256 amount0, uint256 amount1, address to);
     event Mint(address sender, uint256 amount0, uint256 amount1);
 
     /* **** Errors **** */
-    error InsufficientLiquidityMinted();
-    error InsufficientLiquidityBurned();
+    error InsufficientLiquidityMint();
+    error InsufficientLiquidityBurn();
 
     // Possibly change for dynamic naming in the future
     constructor(address _token0, address _token1) ERC20("DexerV2Pair", "DXRLP") {
@@ -52,19 +52,56 @@ contract DexerV2Pair is ERC20 {
         }
 
         if (liquidity < 0) {
-            revert InsufficientLiquidityMinted();
+            revert InsufficientLiquidityMint();
         }
 
         // Mint tokens
         _mint(msg.sender, liquidity);
 
         // Update balances
-        _udpate(balance0, balance1);
+        _update(balance0, balance1);
 
         // Event
         emit Mint(msg.sender, amount0, amount1);
 
         return liquidity;
+    }
+
+    /**
+     * @dev Removes liquidity from the pool by burning LP tokens and transferring the corresponding
+     *      amounts of token0 and token1 to the specified address.
+     * @param to The address to receive the withdrawn amounts of token0 and token1.
+     * @return amount0 The amount of token0 transferred to the `to` address.
+     * @return amount1 The amount of token1 transferred to the `to` address.
+     */
+    function burn(address to) public returns (uint256 amount0, uint256 amount1) {
+        uint256 balance0 = ERC20(token0).balanceOf(address(this));
+        uint256 balance1 = ERC20(token1).balanceOf(address(this));
+
+        uint256 poolLPTokens = balanceOf(address(this));
+
+        if (poolLPTokens == 0) {
+            revert InsufficientLiquidityBurn();
+        }
+
+        uint256 amount0ToTransfer = (poolLPTokens * balance0) / totalSupply();
+        uint256 amount1ToTransfer = (poolLPTokens * balance1) / totalSupply();
+
+        _burn(msg.sender, poolLPTokens);
+
+        // Transfer tokens
+        ERC20(token0).transfer(to, amount0ToTransfer);
+        ERC20(token1).transfer(to, amount1ToTransfer);
+
+        balance0 = ERC20(token0).balanceOf(address(this));
+        balance1 = ERC20(token1).balanceOf(address(this));
+
+        // Update reserves
+        _update(balance0, balance1);
+
+        emit Burn(msg.sender, amount0ToTransfer, amount1ToTransfer, to);
+
+        return (amount0ToTransfer, amount1ToTransfer);
     }
 
     /* **** Getter functions **** */
@@ -74,7 +111,7 @@ contract DexerV2Pair is ERC20 {
 
     /* **** Private functions **** */
 
-    function _udpate(uint256 balance0, uint256 balance1) private {
+    function _update(uint256 balance0, uint256 balance1) private {
         reserve0 = balance0;
         reserve1 = balance1;
     }
