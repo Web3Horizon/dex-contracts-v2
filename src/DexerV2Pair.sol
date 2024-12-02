cb92ef22331e426c53b3c0550bd934c9b2b37d6a
Matheus-OAMK <t2fema00@students.oamk.fi>
2024-12-02 11:35:47 +0200

    refactor: normalize imports syntax for consistency

--- src/DexerV2Pair.sol
+++ src/DexerV2Pair.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
 
 import {IDexerV2Pair} from "src/interfaces/IDexerV2Pair.sol";
