9cafa519da7c4d0e80f80c96a61f928982f2a8d3
Matheus-OAMK <t2fema00@students.oamk.fi>
2024-12-02 11:41:49 +0200

    fix: remove duplicate event and error declarations from IDexerV2Pair interface
    - Removed event and error declarations from the IDexerV2Pair interface.
    - Addressed compilation errors caused by duplicate declarations in the interface and implementation contract.

--- src/interfaces/IDexerV2Pair.sol
+++ src/interfaces/IDexerV2Pair.sol
@@ -2,21 +2,6 @@
 pragma solidity ^0.8.19;
 
 interface IDexerV2Pair {
-    /* **** Events **** */
-    event Burn(address sender, uint256 amount0, uint256 amount1, address to);
-    event Mint(address sender, uint256 amount0, uint256 amount1);
-    event Swap(address indexed sender, uint256 amount0Out, uint256 amount1Out, address indexed to);
-
-    /* **** Errors **** */
-    error DexerV2pair__InsufficientLiquidityMint();
-    error DexerV2pair__InsufficientLiquidityBurn();
-    error DexerV2Pair__InsufficientOutputAmount();
-    error DexerV2Pair__InsufficientInputAmount();
-    error DexerV2Pair__InsufficientLiquidity();
-    error DexerV2Pair__InvalidK();
-    error DexerV2Pair__AlreadyInitialized();
-
-    /* **** Functions **** */
     function initialize(address token0, address token1) external;
 
     function mint() external returns (uint256);
